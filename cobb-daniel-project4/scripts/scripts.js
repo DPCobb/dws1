1b4742be1e88b088489534284d22f75341d5dc32
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2015-11-18 15:26:05 -0500

    css/html edits, **validated
    css comments, styling, html comments, name, source for table info, js
    comments, validated html, cs, js

--- cobb-daniel-project4/scripts/scripts.js
+++ cobb-daniel-project4/scripts/scripts.js
@@ -4,9 +4,11 @@ DWS1 Project4
 Form Interactivity with js*/
 
 function formOn(){
+	//Set variables for welcome message, user name, and alert
 	var welcomeMsg = document.getElementById('welcome');
 	var user = document.getElementById('username').value;
 	var alert = document.getElementById('alert');
+	//Check if username is only letters if true make sub. form visible, add name, hide username/label form hide alert change welcome msg
 	if (/^[a-z, A-Z]+$/.test(user)){
 		document.getElementById('form').style.visibility = "visible";
 		document.getElementById('form').disabled = false;
@@ -16,6 +18,7 @@ function formOn(){
 		welcomeMsg.innerHTML = "Hi, " + user + " thanks for stopping by! Sign up for our newsletter below!";
 		document.getElementById('uname').value = user;
 	}
+	//If false make alert box visible and ask for a name containing only letters
 	else {
 		alert.style.visibility = "visible";
 		alert.innerHTML = "Please enter your name using only letters!";
